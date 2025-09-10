1fc1fdbcbf1678ab4d26b8f227f2bb9db1df0b88
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-09-10 13:55:36 +0900

    WIP: Remove LLVM
    This is not used for except self compiling but it increases build time.
    Thus, this patch remove it.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build.sh
+++ build.sh
@@ -113,9 +113,10 @@ cat << EOS >> conf/local.conf
 INHERIT:remove = "create-spdx"
 IMAGE_INSTALL:append = " git gcc g++ make curl cmake openssl libffi libnsl2"
 IMAGE_INSTALL:append = " binutils patch zlib-dev libffi-dev openssl-dev bzip2 readline sqlite3 ncurses tar patch "
-IMAGE_INSTALL:append = " llvm llvm-dev llvm-staticdev"
 IMAGE_FEATURES += "dev-pkgs tools-sdk"
 IMAGE_INSTALL:append = " zsh vim"
+# For HyCo self compile
+#IMAGE_INSTALL:append = " llvm llvm-dev llvm-staticdev"
 EOS
 fi
 
