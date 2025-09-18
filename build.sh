91d4dc49379ea2bf9d24b61fe59c832ddcac2a2c
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-09-18 15:22:56 +0900

    WIP: Add libdrm-tests package
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build.sh
+++ build.sh
@@ -115,7 +115,7 @@ IMAGE_INSTALL:append = " git gcc g++ make curl cmake openssl libffi libnsl2"
 IMAGE_INSTALL:append = " binutils patch zlib-dev libffi-dev openssl-dev bzip2 readline sqlite3 ncurses tar patch "
 IMAGE_FEATURES += "dev-pkgs tools-sdk"
 IMAGE_INSTALL:append = " opencv"
-IMAGE_INSTALL:append = " zsh vim"
+IMAGE_INSTALL:append = " zsh vim libdrm-tests"
 # For HyCo self compile
 #IMAGE_INSTALL:append = " llvm llvm-dev llvm-staticdev"
 EOS
