e1268f8746916c430b27f345e5116bfa2ad326bd
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-01-28 15:15:53 +0900

    build.sh: Support core-image-weston with next-kernel
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build.sh
+++ build.sh
@@ -122,12 +122,6 @@ cat << EOS >> conf/local.conf
 PREFERRED_VERSION_linux-renesas = "6.18%"
 LINUXLIBCVERSION = "6.18%"
 EOS
-if [[ "${TARGET_IMAGE}" == "core-image-weston" ]];then
-    echo "GPU is not supported in next version kernel so far"
-    # Cleanup symbolic link
-    rm -f ${WORK}/meta-sparrow-hawk
-    exit
-fi
 fi
 
 if [[ "${BUILD_ROOTFS_ONLY}" == "yes" ]]; then
