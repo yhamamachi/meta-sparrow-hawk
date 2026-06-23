507fea72560bf5e63a9370d8969675fc6111bfc6
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-06-23 20:57:55 +0900

    build.sh: Remove SPDX_ARCHIVE_PACKAGED to reduce deploy/spdx size
    This option introduces the all files contained binary into SPDX.
    But, we don't need binary files.
    So, this patch removes this option.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build.sh
+++ build.sh
@@ -120,8 +120,6 @@ SPDX_INCLUDE_SOURCES = "1"
 # optional. if "1", bitbake will create source files archive for each package.
 SPDX_ARCHIVE_SOURCES = "1"
 
-# optional. if "1", bitbake will create output binary archive for each package.
-SPDX_ARCHIVE_PACKAGED = "1"
 EOS
 else # Disable SBOM build to reduce build time
 cat << EOS >> conf/local.conf
